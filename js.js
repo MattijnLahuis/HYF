8cb89d3746d2068aa510772982399a6c4a96f12d
Mattijn Lahuis <mattijnlahuis@gmail.com>
2017-05-21 14:32:14 +0200

    blue background

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("This javascript is super useful.");
+
+  document.getElementById('mainDiv').style['background-color'] = 'blue'
 });
